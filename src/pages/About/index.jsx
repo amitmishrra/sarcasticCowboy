09956d604c8ebc6fac98edd71aea637d56d71ddd
mishra4amit4@gmail.com <mishra4amit4@gmail.com>
2022-11-11 03:25:49 +0530

    about page fixes

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,24 +1,40 @@
 import "./styleA.css"
 import { NavLink } from "react-router-dom"
+import React, { useState } from 'react'
+import { useEffect } from 'react';
 
 const About = () => {
+    const [first, setFirst] = useState(0)
+    const [second, setSecond] = useState(first+1)
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            first === 3 ? setFirst(0) : setFirst(first + 1);
+            second === 3 ? setSecond(0) : setSecond(second + 1);
+        }, 3000)
+        return () => clearInterval(intervalId);
+    }, [first])
+
     return (
         <>
-            <main className="pt-[100px]">
+            <main className="pt-[60px]">
                 <section className="frstSec flex flex-col md:flex-row items-center justify-center md:justify-between">
-                    <div className="frst relative flex items-center justify-center">
-                        <img src="assets/about.jpg" className="imge1" alt="" />
-                        <img src="assets/home2.jpg" className="imge2 absolute" alt="" />
-                    </div>
-                    <div className="scnd flex flex-col items-start justify-center">
-                        <h1>Sarcastic Cowboy</h1>
-                        <p>
+
+                    <div className="scnd flex flex-col items-start justify-center w-[1/2]">
+                        <h1  className="md:text-[70px] text-[40px]">Sarcastic Cowboy</h1>
+                        <p className="md:text-[25px] text-[20px] md:mt-6 mt-[6+
+                            px]">
                             Hi and Welcome to Sarcastic Cowboy.
                             I am a blogger, copywriter, memer, self-acclaimed cricket expert, and movie buff.
                         </p>
                     </div>
+
+                    <div className="frst relative flex items-center justify-center mt-[30px] md:mt-0">
+                        <img src={`assets/aboutPage/about${first}.jpg`} className="imge1 borderShadow" alt="" />
+                        <img src={`assets/aboutPage/about${second}.jpg`} className="imge2 absolute borderShadow invisible md:visible" alt="" />
+                    </div>
+
                 </section>
-                <section className="scndSec">
+                <section className="scndSec text-[20px]">
                     <p>
                         The full-time job that earns me money is about finding bugs and helping them get eliminated. No, you got it absolutely wrong I don’t work in a pest control company. Instead, I work in one of the leading IT companies located in India as a Senior Software Development Engineer in Test(SDET)  where my job is to code and find defects in the other’s code. This is all about my 9-6 job
                         In part-time when bored with coding, I love to write, my interest in writing developed way back in school from writing love letters to my crush and it has continued till today. I have been funny and love to crack jokes since childhood and this reflects in my writing too. I mainly write jokes, create memes with only intent to make people laugh as I believe writing comedy always connects people easily, puts them in a better mood, gives them chance to unwind and relax.
@@ -38,7 +54,7 @@ const About = () => {
                             Thanks for being here !
                         </h1>
                         <p>
-                            I just want to end up by saying that I hope you find this blog useful, funny, and informative. If you have any questions, comments or feedback reach out to me via 
+                            I just want to end up by saying that I hope you find this blog useful, funny, and informative. If you have any questions, comments or feedback reach out to me via
                             <NavLink to="/contact" className=" text-blue-500"> Contact Us</NavLink> page
                         </p>
                     </div>
