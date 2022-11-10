b604fab1169fb6f7c09033a895509b9359a68efd
Farhan akhtar <sheikhfarhan722@gmail.com>
2022-11-10 16:50:56 +0530

    homepage initialized

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,32 @@
+import "./style.css"
+
 const Home = () => {
     return (
         <>
-        <main className="pt-[70px]">
-            <section className=" flex flex-col justify-around items-center h-full w-full ">
-
-            </section>
-        </main>
+            <main className="pt-[70px]">
+                <section className=" flex md:flex-row flex-col items-center justify-center">
+                    <div className="frst flex items-center justify-center">
+                        <div>
+                            <div className="txtGrp">
+                                <h2>Blogger. Copywriter. Memer.</h2>
+                                <h1>Sarcastic Cowboy</h1>
+                            </div>
+                            <div className="btnGrp flex flex-col items-center md:items-start">
+                                <a href=""><button>Know More</button></a>
+                                <div className="social">
+                                    <i>f</i>
+                                    <i>t</i>
+                                    <i>i</i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="scnd relative flex items-center justify-center">
+                        <img src="assets/home1.jpg" className="img1" alt="" />
+                        <img src="assets/home2.jpg" className="img2 absolute" alt="" />
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
